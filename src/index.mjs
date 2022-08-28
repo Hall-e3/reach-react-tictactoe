2ee4b25262f0f05629bce73558caff82e97d3c87
Hall Enoch Asanda <hallenochasanda@gmail.com>
2022-08-28 09:27:02 +0300

    index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -1,27 +1,75 @@
-import {loadStdlib} from '@reach-sh/stdlib';
-import * as backend from './build/index.main.mjs';
-const stdlib = loadStdlib(process.env);
 
-const startingBalance = stdlib.parseCurrency(100);
+import { loadStdlib } from "@reach-sh/stdlib";
+import * as backend from "./build/index.main.mjs";
+const reach = loadStdlib(process.env);
 
-const [ accAlice, accBob ] =
-  await stdlib.newTestAccounts(2, startingBalance);
-console.log('Hello, Alice and Bob!');
+const startingBalance = reach.parseCurrency(100);
 
-console.log('Launching...');
+const [accAlice, accBob] = await reach.newTestAccounts(2, startingBalance);
+console.log("Hello, Deployer and Api!");
+
+console.log("Launching...");
 const ctcAlice = accAlice.contract(backend);
-const ctcBob = accBob.contract(backend, ctcAlice.getInfo());
 
-console.log('Starting backends...');
-await Promise.all([
-  backend.Alice(ctcAlice, {
-    ...stdlib.hasRandom,
-    // implement Alice's interact object here
-  }),
-  backend.Bob(ctcBob, {
-    ...stdlib.hasRandom,
-    // implement Bob's interact object here
-  }),
-]);
+const API = async (name) => {
+  const acc = await reach.newTestAccount(startingBalance);
+  const ctc = acc.contract(backend, await ctcAlice.getInfo());
+  const acceptWager = async () => {
+    try {
+      await ctc.apis.Players.acceptWager();
+      console.log(`${name} accepted wager`);
+    } catch (error) {
+      console.log("Error");
+    }
+  };
 
-console.log('Goodbye, Alice and Bob!');
+  return { acceptWager };
+};
+
+
+class Admins {
+  constructor(info, acct) {
+    this.acc = acct ?? reach.newTestAccount(startingBalance);
+    this.ctc = this.acc.contract(backend, info);
+  }
+  winner = async (winner) => {
+    try {
+      const winarr = ["Player1", "Player2"];
+      await this.ctc.apis.Admin.winner(winner);
+      console.log(` The winner is ${winarr[winner % 2]}`);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+}
+
+console.log("Starting backends...");
+try {
+  await Promise.all([
+    backend.Deployer(ctcAlice, {
+      wager: reach.parseCurrency(1),
+      ready: async () => {
+        console.log(
+          "Contract is ready to be attached to\n Contract info:",
+          await ctcAlice.getInfo()
+        );
+        throw 42;
+      },
+      // implement Deployer's interact object here
+    }),
+  ]);
+} catch (error) {
+  console.log(error);
+}
+
+const info = await ctcAlice.getInfo();
+const Adm = new Admins(info, accAlice);
+const P1 = await API("Player1");
+const P2 = await API("Player2");
+
+await P1.acceptWager();
+await P2.acceptWager();
+
+await Adm.winner(0);
+
+console.log("Goodbye, Deployer and Api!");
--- src/index.rsh
+++ src/index.rsh
@@ -1,19 +1,67 @@
-'reach 0.1';
+"reach 0.1";
+// 2players connect
+// Someone sends the outcome to the backend and total is sent to the winner
+const Player = {
+  getValue: Fun([], UInt),
+  seeChoice: Fun([UInt], Null),
+};
 
 export const main = Reach.App(() => {
-  const A = Participant('Alice', {
-    // Specify Alice's interact interface here
+  const Deployer = Participant("Deployer", {
+    ready: Fun([], Null),
+    wager: UInt,
   });
-  const B = Participant('Bob', {
-    // Specify Bob's interact interface here
+  const Players = API("Players", {
+    acceptWager: Fun([], Null),
+  });
+  const Admin = API("Admin", {
+    winner: Fun([UInt], Null),
   });
   init();
-  // The first one to publish deploys the contract
-  A.publish();
+
+  Deployer.only(() => {
+    const wager = declassify(interact.wager);
+  });
+
+  Deployer.publish(wager);
+
+  Deployer.interact.ready();
+
+  const [end, amt, howMany, addrs] = parallelReduce([
+    false,
+    0,
+    0,
+    Array.replicate(2, Deployer),
+  ])
+    .invariant(balance() == amt)
+    .while(!end)
+    .api(
+      Players.acceptWager,
+      () => {
+        check(howMany < 2);
+      },
+      () => wager,
+      (k) => {
+        const addr = addrs.set(howMany % 2, this);
+        k(null);
+        return [end, amt + wager, howMany + 1, addr];
+      }
+    )
+    .api(
+      Admin.winner,
+      (_) => {
+        check(this == Deployer);
+        check(howMany == 2);
+      },
+      (_) => 0,
+      (win, k) => {
+        transfer(balance()).to(addrs[win % 2]);
+        k(null);
+        return [true, 0, howMany, addrs];
+      }
+    );
+
+  transfer(balance()).to(Deployer);
   commit();
-  // The second one to publish always attaches
-  B.publish();
-  commit();
-  // write your program here
   exit();
 });
